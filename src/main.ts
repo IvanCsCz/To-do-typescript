02c730d741b35a26a29dcc7277d06dc39868b0a0
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-08-22 02:25:16 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style.css'
+
+const list = document.querySelector<HTMLUListElement>('#list')
+const form = document.querySelector<HTMLFormElement>('#new-task-form')
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
